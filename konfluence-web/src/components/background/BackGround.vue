c20a120882fb143ed3fbe83155677550ecf979b2
Kaspar Kivistik <kkivis@ttu.ee>
2019-02-02 18:37:19 +0200

    Improved travis script

--- konfluence-web/src/components/background/BackGround.vue
+++ konfluence-web/src/components/background/BackGround.vue
@@ -1,5 +1,18 @@
 <template>
-  <Bubble />
+  <div>
+    <Bubble
+      color="blue"
+      cx="50"
+      cy="40"
+      r="90"
+    />
+    <Bubble
+      color="yellow"
+      cx="30"
+      cy="50"
+      r="30"
+    />
+  </div>
 </template>
 
 <script>
