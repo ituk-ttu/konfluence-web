344b946a22ec57d17ba2f1c5bfb9f9f2ad6b76ea
Kaspar Kivistik <kkivis@ttu.ee>
2019-02-16 17:07:43 +0200

    fixed typo

--- konfluence-web/src/components/ic_components/Speakers/Speakers.js
+++ konfluence-web/src/components/ic_components/Speakers/Speakers.js
@@ -4,7 +4,7 @@ export const speakers = [
     image: 'https://scontent.fbud2-1.fna.fbcdn.net/v/t31.0-8/24059750_1614230071949820_2202051073901494972_o.jpg?_nc_cat=108&_nc_ht=scontent.fbud2-1.fna&oh=05293b652c1c6768aaa5508c011e6ae7&oe=5CE601BA',
     name: 'Kaspar Kivistik',
     company: 'Monese',
-    text: 'Alustasin oma teekonda Levercodeist. Nüüd olen moniisis. Mu arvuti on Apple. Mu lemmik kool on TalTech ja mu lemmik inimene on Emily Joy Krohl. Mis on sinu lemmikud?'
+    text: 'Alustasin oma teekonda Levercodeist. Nüüd olen moniisis. Mu arvuti on Apple. Mu lemmik kool on TalTech ja mu lemmik inimene on Emily Joy Krohn. Mis on sinu lemmikud?'
   },
   {
     image: 'http://www.samhurdphotography.com/wp-content/uploads/2014/06/dc-celebrity-portrait-photographers.jpg',
