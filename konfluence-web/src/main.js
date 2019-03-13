09244e0ac41ab4b5785fc9bf6af8be53fd31c6e0
Kaspar Kivistik <kkivis@ttu.ee>
2019-03-13 18:23:11 +0200

    weirdass commit just so Travis can pull the new stuffs in

--- konfluence-web/src/main.js
+++ konfluence-web/src/main.js
@@ -2,22 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'expose-loader?$!expose-loader?jQuery!jquery'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope, faHeart, faCoffee, faCopyright, faGlobe, faMapMarkerAlt, faBus, faPhone } from '@fortawesome/free-solid-svg-icons'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faEnvelope, faHeart, faCoffee, faCopyright, faGlobe, faMapMarkerAlt, faBus, faPhone} from '@fortawesome/free-solid-svg-icons'
+import {faFacebook} from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+import VueScrollTo from 'vue-scrollto'
 
 library.add(faFacebook, faEnvelope, faHeart, faCoffee, faCopyright, faGlobe, faMapMarkerAlt, faBus, faPhone);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
-
-const VueScrollTo = require('vue-scrollto');
 Vue.use(VueScrollTo, {
-  container: "body",
+  container: 'body',
   duration: 2000,
-  easing: "ease",
+  easing: 'ease',
   offset: -100,
   force: true,
   cancelable: true,
@@ -29,7 +29,7 @@ Vue.use(VueScrollTo, {
 });
 
 Vue.use({
-  install: function(Vue, options){
+  install: function (Vue, options) {
     Vue.prototype.$jQuery = require('jquery'); // you'll have this.$jQuery anywhere in your vue project
   }
 });
