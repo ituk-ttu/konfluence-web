a3ae06c3ad70e4dd706d508b2eed04aa49f8e215
Raimond Lume <lume.raimond@gmail.com>
2019-03-28 09:38:07 +0200

    add more space for overflow gradient

--- konfluence-web/src/components/ic_components/Speakers/Speakers.js
+++ konfluence-web/src/components/ic_components/Speakers/Speakers.js
@@ -3,7 +3,7 @@ export const speakers = [
     image: 'https://i.imgur.com/aZHNJFB.jpg',
     name: 'Margus Holland',
     company: 'LHV',
-    text: 'Margus Holland on erinevaid kasutajaliideseid disaininud ja arendanud enam kui 15 aastat, pakkudes töötavaid lahendusi klientidele nii freelance vormis kui tööandja juures. Marguse praegune tööandja on LHV Pank, kus tema ülesandeks igapäevaselt on mobiili- ja internetipanga tootejuhtimine ühelt poolt ning digitaalpanganduse tiimi juhtimine teiselt. Varasemalt on ta vedanud Skype’s skype.com veebitiimi ning Helmeses töötanud kasutajaliideste arendajana/disainerina. Vabal ajal tegeleb ta lisaks muudele asjadele mängude arendusega Apple iOS platvormile.'
+    text: 'Margus Holland on erinevaid kasutajaliideseid disaininud ja arendanud enam kui 15 aastat, pakkudes töötavaid lahendusi klientidele nii freelance vormis kui tööandja juures. Marguse praegune tööandja on LHV Pank, kus tema ülesandeks igapäevaselt on mobiili- ja internetipanga tootejuhtimine ühelt poolt ning digitaalpanganduse tiimi juhtimine teiselt. Varasemalt on ta vedanud Skype’s skype.com veebitiimi ning Helmeses töötanud kasutajaliideste arendajana/disainerina. Vabal ajal tegeleb ta lisaks muudele asjadele mängude arendusega Apple iOS platvormile.\n'
   },
   {
     image: 'https://i.imgur.com/QkB1bZN.png',
@@ -15,6 +15,6 @@ export const speakers = [
     image: 'https://i.imgur.com/J23FdT2.png',
     name: 'Vaiko Vahter',
     company: 'Finestmedia AS juhtivarendaja',
-    text: 'Vaiko on tarkvaraarendusega tegelenud kuus aastat. Kõige rohkem pakuvad talle huvi keerukad loogikaülesanded. Vabal ajal proovib ta aega leida kodu automatiseerimise jaoks ning kui ta juhuslikult arvuti taga ei ole, siis mängib sõpradega lauamänge või reisin seljakotiga maailmas ringi.'
+    text: 'Vaiko on tarkvaraarendusega tegelenud kuus aastat. Kõige rohkem pakuvad talle huvi keerukad loogikaülesanded. Vabal ajal proovib ta aega leida kodu automatiseerimise jaoks ning kui ta juhuslikult arvuti taga ei ole, siis mängib sõpradega lauamänge või reisin seljakotiga maailmas ringi.\n'
   }
 ];
